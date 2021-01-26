c8a5dd99825f69cbcd950a4868678baba9a972a7
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2021-01-26 11:54:08 +0100

    chore: move code to partial class

--- src/lowlandtech/Program.cs
+++ src/lowlandtech/Program.cs
@@ -16,6 +16,7 @@ namespace LowlandTech
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddAdminLte();
+
             await builder.Build().RunAsync();
         }
     }
---
+++ src/lowlandtech/Shared/MainLayout.razor.cs
@@ -0,0 +1,48 @@
+﻿using Blazor.AdminLte;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using System;
+
+namespace LowlandTech.Shared
+{
+    public partial class MainLayout
+    {
+        [Inject] public NavigationManager NavigationManager { get; set; }
+        [Inject] public IJSRuntime Js { get; set; }
+        [Inject] public ILayoutManager LayoutManager { get; set; }
+        [Inject] public NavBarLeftInjectableMenu NavBarLeftInjectableMenu { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            // content = null; for refreshing custom menu injectables per page.
+            // no override; reset default to show parent menu content items.
+            NavBarLeftInjectableMenu.SetContent(null, false);
+        }
+
+        private readonly DateTime _dt = DateTime.Now;
+
+        private void SeeAllMessages(Tuple<IDropdownFooter, MouseEventArgs> args)
+        {
+            NavigationManager.NavigateTo("messages/see-all-messages");
+        }
+
+        private void HandleContent(Tuple<INavBarMenuItem, MouseEventArgs> args)
+        {
+            NavigationManager.NavigateTo("messages/see-all-messages");
+        }
+
+        private void SeeAllNotifications(Tuple<IDropdownFooter, MouseEventArgs> args)
+        {
+            NavigationManager.NavigateTo("messages/see-all-notifications");
+        }
+
+        protected override void OnAfterRender(bool isFirstRender)
+        {
+            LayoutManager.IsFooterFixed = true;
+            LayoutManager.IsNavBarFixed = true;
+            LayoutManager.IsSideBarFixed = true;
+            base.OnAfterRender(isFirstRender);
+        }
+    }
+}
